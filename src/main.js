2183daaacd67c7aa5119dece040a64c19fe8083e
Julien Simonet <j.mons54@gmail.com>
2020-11-19 17:24:43 +0100

    add material kit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import VueMaterial from 'vue-material'
+
+import 'vue-material/dist/vue-material.min.css'
+import '@/assets/scss/material-kit.scss'
+import '@/assets/scss/app.scss'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
