df2ba449b8f3eaece90da4d49529f1922788c85d
Julien Simonet <j.mons54@gmail.com>
2020-11-20 05:18:50 +0100

    bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -7,50 +7,59 @@
       to="/"
       class="logo">
       <img src="@/assets/logo.svg">
-      <span class="primary--text">Bi</span>
-      <span>Star</span>
+      <span class="primary--text">Cha</span>
+      <span>Bit</span>
     </router-link>
+    <v-toolbar-items
+      v-if="!isMobile"
+      class="ml-8">
+      <v-btn
+        text>
+        {{ $t('markets') }}
+      </v-btn>
+      <v-btn
+        v-if="!isMobile"
+        text>
+        {{ $t('trade') }}
+      </v-btn>
+    </v-toolbar-items>
 
     <v-spacer></v-spacer>
 
-    <div v-if="!isMobile">
+    <v-toolbar-items>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        class="mr-4"
+        v-if="!isMobile"
         text>
         {{ $t('login') }}
       </v-btn>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        class="mr-4"
-        depressed>
+        v-if="!isMobile">
         {{ $t('signup') }}
       </v-btn>
-    </div>
-
-    <v-menu
-      offset-y
-      open-on-hover>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          dark
-          v-bind="attrs"
-          v-on="on"
-          text>
-          {{ language.name }}
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item-group>
-          <v-list-item
-            v-for="(language, key) in languages"
-            :key="key"
-            :href="`/${language.code}${$route.fullPath}`">
-            <v-list-item-title>{{ language.name }}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-menu>
+      <v-menu
+        offset-y
+        open-on-hover>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            dark
+            v-bind="attrs"
+            v-on="on"
+            text>
+            {{ language.name }}
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item-group>
+            <v-list-item
+              v-for="(language, key) in languages"
+              :key="key"
+              :href="`/${language.code}${$route.fullPath}`">
+              <v-list-item-title>{{ language.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
   </v-app-bar>
 </template>
 
--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,23 +1,15 @@
 <template>
   <v-bottom-navigation
     fixed
-    color="primary"
-  >
-    <v-btn>
-      <span>{{ $t('home') }}</span>
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-
+    color="primary">
     <v-btn>
       <span>{{ $t('markets') }}</span>
       <v-icon>mdi-distribute-horizontal-center</v-icon>
     </v-btn>
-
     <v-btn>
       <span>{{ $t('trade') }}</span>
       <v-icon>mdi-swap-vertical-bold</v-icon>
     </v-btn>
-
     <v-btn>
       <span>{{ $t('wallet') }}</span>
       <v-icon>mdi-wallet</v-icon>
